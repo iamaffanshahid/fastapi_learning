5229bbdbee4dc93bd2f424d6ec5d584a77d96f01
affan ansari <affananxari839@gmail.com>
2024-09-18 16:35:29 +0500

    revision_course

---
+++ python_revision/user_input.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+first_name = input("Enter your first name: ")
+days = int(input("how many days until your birthday: "))
+weeks = int(days/7)
+print(f"hi {first_name},only {weeks} weeks before your birthday! ")
