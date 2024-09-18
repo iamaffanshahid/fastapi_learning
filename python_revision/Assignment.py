2ddc54b9d29cff6c31e9d185b34903cb70e59196
affan ansari <affananxari839@gmail.com>
2024-09-18 15:25:51 +0500

    revision_course

---
+++ python_revision/Assignment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+going to find
+out the 
+remaining from 
+total amount
+"""
+total_amount = 50
+cost =  15
+tax  =  cost*.25
+total_cost = cost + tax
+remaining = total_amount - total_cost
+
+print(remaining)
---
+++ string_formating.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+first_name = "Umer"
+last_name  =  "Javid"
+print(f"Hi {first_name} {last_name} here is my asssignment work")
+
+sentence = "Hi {} {}"
+print(sentence.format(first_name, last_name))
