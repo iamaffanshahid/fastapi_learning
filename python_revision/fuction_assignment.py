f107fc833894f9474aea04f5b564dc60ec8337d7
affan ansari <affananxari839@gmail.com>
2024-09-19 14:58:53 +0500

    function_assignment

---
+++ python_revision/fuction_assignment.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def dictionary(firstname,lastname,age):
+    dictionary = {
+        "firstname": firstname,
+        "lastname": lastname,
+        "age"     : age
+        }
+    return dictionary    
+
+solution_dictiondeary = dictionary(firstname="ALi",lastname="Murtzza",age=30)
